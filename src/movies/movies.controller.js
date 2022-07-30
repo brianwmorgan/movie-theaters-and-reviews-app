c7f042f25c9bcb744af71362402f2bc178a0530b
Brian Morgan <brianwmorgan317@gmail.com>
2022-07-30 06:59:22 -0400

    refactored list() and read() functions in the 'movies' controller to use try/catch blocks instead of asyncErrorBoundary()

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,31 +1,40 @@
 const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+//  *** refactor to use try/catch ??? ***
 async function validateMovieId(req, res, next) {
-    const { movieId } = req.params;
-    const movie = await moviesService.read(movieId);
-    if(movie) {
-        res.locals.movie = movie;
-        return next();
-    };
-    next({
-        status: 404,
-        message: "Movie cannot be found.",
-    });
-};
+  const { movieId } = req.params;
+  const movie = await moviesService.read(movieId);
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({
+    status: 404,
+    message: "Movie cannot be found.",
+  });
+}
 
 async function list(req, res, next) {
+  try {
     const data = await moviesService.list(req.query.is_showing);
     res.json({ data });
-};
+  } catch (error) {
+    next(error);
+  }
+}
 
 async function read(req, res, next) {
+  try {
     const { movieId } = req.params;
     const data = await moviesService.read(movieId);
     res.json({ data });
-};
+  } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = {
-    list: [asyncErrorBoundary(list)],
-    read: [asyncErrorBoundary(validateMovieId), asyncErrorBoundary(read)],
+  list,
+  read: [asyncErrorBoundary(validateMovieId), read],
 };
